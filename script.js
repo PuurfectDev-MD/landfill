998a8123cc125dbe3c667d71f0bc6b373fef2a66
PuurfectDev-MD <new.manishd@gmail.com>
2026-02-15 12:24:11 +0545

    added npm vite keeping functionalities

---
+++ script.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+const theme_btn = document.getElementById("theme_btn");
+const theme_btn_img = document.getElementById("theme_btn_img");
+
+theme_btn.addEventListener("click", () => {
+    // THIS LINE IS THE KEY: It tells Tailwind to switch to dark mode
+    document.documentElement.classList.toggle("dark");
+
+    if (theme_btn_img.src.includes("light.png")) {
+        theme_btn_img.src = "/assets/images/dark.png";
+        console.log("theme turned to dark");
+    } else {
+        theme_btn_img.src = "/assets/images/light.png";
+        console.log("theme turned to light");
+    }
+});
