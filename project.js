c725403911386bb726735060ee1120e5e2b69941
PuurfectDev-MD <new.manishd@gmail.com>
2026-02-17 09:56:10 +0545

    Added projects

---
+++ project.js
@@ -0,0 +1,28 @@
+// projects info
+
+export const projects =[
+    {
+        title:"Pawpad",
+        description: "A cool looking paw shaped macropad for cat people",
+        link: "#project1",
+        tags: ["pawpad", "macropad", "cat", "electronics", "pcb"]
+    },
+    {
+        title: "Meow-Meow-purr",
+        description: "A wacky file storage project which converts any img to cat sounds",
+        link: "#project2",
+        tags: ["meow meow", "cat", "python", "image"]
+    },
+    {
+        title: "Near365",
+        description: "A gift idea for a person",
+        link: "#project2",
+        tags: ["meow meow", "cat", "python", "image"]
+    },
+    {
+        title: "This very website",
+        description: "Personal website to reflex me and store my wacky-cool-creative projects whatver they may be",
+        link: "#project2",
+        tags: ["meow meow", "cat", "python", "image"]
+    }
+]
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import './style.css';
+import  {projects} from './project'
 
 const main_pg_drawings = document.querySelectorAll(".project-drawing");
 
@@ -18,6 +19,22 @@ if (main_pg_drawings.length >0){  /*checks if drawings exist and then listens fo
 
 }
 
+
+// for project page
+
+const projectGrid = document.getElementById("project-grid")
+if (projectGrid){
+  const projectCards = projects.map(project =>
+    ` <div class="bg-card-background p-6 rounded-2xl shadow-amber-300 hover:bg-card-hover hover:scale-105 transition-all border border-card-hover border-2 text-card-text">
+           <a href= "${project.url}">
+            <h2 class="font-bitcountGridDouble_medium text-2xl">${project.title}</h2>
+            <p class="py-4 font-schoolbell">${project.description}</p>
+            </a>
+        </div>`
+  ).join('')
+  
+    projectGrid.innerHTML = projectCards
+}
 
 
 
