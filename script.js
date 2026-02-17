090ba5fca7175d1c56e922e20230883eac50eecb
PuurfectDev-MD <new.manishd@gmail.com>
2026-02-17 22:26:27 +0545

    Added animated lights to home and edited contact

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 import './style.css';
 import  {projects} from './project'
 
-const main_pg_drawings = document.querySelectorAll(".project-drawing");
 
+
+const main_pg_drawings = document.querySelectorAll(".project-drawing");
+const svg_item = document.querySelector('svg')
+
+if (svg_item){
+  window.addEventListener('load', () => {
+    const path =svg_item.querySelector("path")
+    if (!path) return
+
+
+    const totalLength = path.getTotalLength();
+    const numDots =20;
+
+    for(let i=0; i<numDots; i++){
+      const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+      const distance = Math.random() * totalLength
+
+      const point = path.getPointAtLength(distance)
+
+      circle.setAttribute("cx", point.x)
+      circle.setAttribute("cy", point.y)
+
+      circle.setAttribute("r", Math.random()*8 +5)
+      var color = Math.floor(Math.random()*3)
+      var colorValue
+      if(color ==0){
+          colorValue= "oklch(51.4% 0.222 16.935)" 
+        }else if (color ==1){
+          colorValue ="oklch(62.7% 0.194 149.214)"
+        }else{
+          colorValue = "oklch(54.6% 0.245 262.881)"
+        }
+
+      circle.setAttribute("fill", colorValue )
+      circle.classList.add("flicker-dot")
+      circle.style.animationDelay = `${Math.random() *2}s`
+
+      svg_item.appendChild(circle)
+    }
+  })
+}
 
 if (main_pg_drawings.length >0){  /*checks if drawings exist and then listens for events  -this way the js doesnt crash in a different page */
     main_pg_drawings.forEach((drawing) =>{
